67ac938f78aa077a323c85ff79f499ae1fc2f728
victorhdcoelho <victorhdcoelho@gmail.com>
2018-03-23 09:52:18 -0300

    Fix bug in first item and bug in python2

--- find_like_a_god.py
+++ find_like_a_god.py
@@ -1,4 +1,4 @@
-""" Python version 2 """
+""" Python version 3.6.3 """
 from random import randint
 import os
 import time
@@ -6,13 +6,31 @@ import timeit
 max_len = 100
 
 def mix_search(list_db,num_search):
+    tic = timeit.default_timer()
     insertion_sort(list_db)
+    toc = timeit.default_timer()
+    print("-------------------------------------------------")
+    print("| Time insertion_sort: {}    |".format(toc-tic))
+    print("-------------------------------------------------")
+    tic = timeit.default_timer()
     list_index = index_list(list_db)
+    toc = timeit.default_timer()
+    print("| Time index_list: {}        |".format(toc-tic))
+    print("----------------------------------------------------------")
+    tic = timeit.default_timer()
     end , begin = sequencial_search(num_search,list_index)
-    print("end: {} begin: {}".format(end,begin))
+    toc = timeit.default_timer()
+    print("| Time sequencial_search in index_list: {} |".format(toc-tic))
+    print("---------------------------------------------------------------")
+    print("| Sub set list_db end: {} begin: {}              |".format(end,begin))
+    print("-------------------------------------------------")
     if not(end or begin):
         return -1
+    tic = timeit.default_timer()
     number = search_binary(list_db , num_search, begin, end)
+    toc = timeit.default_timer()
+    print("| Time search_binary: {}     |".format(toc-tic))
+    print("-------------------------------------------------")
     return number
 
 def insertion_sort(list_db = []):
@@ -31,7 +49,6 @@ def index_list(list_db = []):
     index = [index-1 for index in range(0,max_len,10)]
     index.append(max_len-1)
     index[0]+=1
-    print(index)
     list_values = [list_db[code] for code in index]
     list_values = list(zip(list_values,index))
     return list_values
@@ -39,7 +56,9 @@ def index_list(list_db = []):
 def sequencial_search(num_search,list_num):
     count = 0
     for item in list_num:
-        if item[0] == num_search or item[0] > num_search:
+        if item[0] >= num_search:
+            if item[1] == 0:
+                return 9,0
             return item[1] , list_num[count-1][1]
         count+=1
     return -1,-1
@@ -105,22 +124,26 @@ if __name__ == "__main__":
 
     path = os.path.dirname(os.path.abspath(__file__))
     new_path = os.path.join(path,"db.txt")
-
-    if os.stat(new_path).st_size == 0 :
+    try:
+        db = open(new_path,"r")
+    except:
         db = open("db.txt","w+")
         for count in range(0,max_len):
             number = randint(0,99999)
             db.write("%d \n" %(number))
         db.close()
-
-    db = open("db.txt","r")
+    db = open(new_path,"r")
     list_db = init_list(db)
     answer = 10
     while answer > 0:
-        print("Choose one of choises bellow:\n1-Print unorder list.\n2-Print order list.\n3-Find a number.\n4-Mix search\n0-Getout here.")
+        print("Choose one of choises bellow:\n1-Print unorder list.\n2-Print order list.\n3-Binary search.\n4-Mix search\n0-Getout here.")
         answer = int(input())
         choise(answer,list_db)
-        print("Press enter to continue!!!")
-        raw_input()
+        print("Press enter to continue!!!")        
+        try:
+            input()
+        except:
+            print("You are in python2 please press enter again !!!")
+            raw_input()
         #time.sleep(3)
         os.system("clear")
